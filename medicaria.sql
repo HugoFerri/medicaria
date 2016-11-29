0b971831ec942419bfeb32122b30b7f02ee775c4
Rafael Aznar <rafaaznar@gmail.com>
2016-11-29 16:41:08 +0100

    Update medicaria.sql

--- medicaria.sql
+++ medicaria.sql
@@ -57,48 +57,11 @@ INSERT INTO `importancia` (`id`, `descripcion`) VALUES
 (19, 'mortal'),
 (20, 'desconocida');
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `importancia`
---
 ALTER TABLE `importancia`
   ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `importancia`
 --
 ALTER TABLE `importancia`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
--- phpMyAdmin SQL Dump
--- version 4.6.3
--- https://www.phpmyadmin.net/
---
--- Servidor: localhost:3306
--- Tiempo de generación: 29-11-2016 a las 07:00:16
--- Versión del servidor: 5.5.50
--- Versión de PHP: 5.5.38
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `medicaraDB`
---
-
--- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `medicamento`
