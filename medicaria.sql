ec7eaf6b5480fa7272e5fec101205f745a556010
a073659292k <a073659292k@s09-pc05>
2016-11-29 16:10:09 +0100

    Tabal via (Alvaro)

--- medicaria.sql
+++ medicaria.sql
@@ -4,3 +4,20 @@
 -- Sistema de información Sanitario
 -- ---------------------------------
 
+CREATE TABLE via (
+id INT AUTO_INCREMENT PRIMARY KEY,
+descripcion VARCHAR(255) NOT NULL,
+id_medicamento INT NOT NULL, 
+);
+
+INSERT INTO via (descripcion, id_medicamento)
+VALUES ("Via oral",1),
+("Via sublingual",2),
+("Via topica",3),
+("Via transdermica",4),
+("Via oftalmica",5),
+("Via otica",6),
+("Via intranasal",7),
+("Via intravenosa",8),
+("Via intramuscular",9),
+("Via subcutanea",10);
