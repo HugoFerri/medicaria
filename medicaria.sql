3b49cfd075c16fcdd866555d21556a36fe8e90ec
a021008414f <a021008414f@s09-pc02>
2016-11-29 16:53:36 +0100

    Commit de la tabla paciente sin comentarios arreglado

--- medicaria.sql
+++ medicaria.sql
@@ -1,20 +1,11 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
---
--- Estructura de tabla para la tabla `paciente`
---
+-------------------------------------------------------------------------------------------------
 
 CREATE TABLE `paciente` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(255) NOT NULL,
-  `primer_apellido` varchar(255) NOT NULL,
-  `segundo_apellido` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `paciente`
---
+  `nombre` varchar(255),
+  `primer_apellido` varchar(255),
+  `segundo_apellido` varchar(255)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 INSERT INTO `paciente` (`id`, `nombre`, `primer_apellido`, `segundo_apellido`) VALUES
 (1, 'Daniel', 'Garcia', 'Marquez'),
@@ -30,25 +21,10 @@ INSERT INTO `paciente` (`id`, `nombre`, `primer_apellido`, `segundo_apellido`) V
 (11, 'Francisco', 'Castro', 'Martin'),
 (12, 'Emilio', 'Gaviria', 'Romero');
 
---
--- Índices para tablas volcadas
---
 
---
--- Indices de la tabla `paciente`
---
 ALTER TABLE `paciente`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `paciente`
---
 ALTER TABLE `paciente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
