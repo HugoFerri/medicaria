a203b30bc5ac0fe537a55f638ad5d0658b652791
a026749314s <a026749314s@s09-pc21>
2016-11-29 16:03:51 +0100

    table importancia created and filled

--- medicaria.sql
+++ medicaria.sql
@@ -4,3 +4,34 @@
 -- Sistema de información Sanitario
 -- ---------------------------------
 
+CREATE TABLE `test`.`importancia` 
+  ( 
+     `id`          INT NOT NULL auto_increment, 
+     `descripcion` VARCHAR(255) NOT NULL, 
+     PRIMARY KEY (`id`) 
+  ) 
+engine = innodb;
+
+INSERT INTO `importancia` 
+            (`id`, 
+             `descripcion`) 
+VALUES      (NULL, 
+             'irrelevante'), 
+            (NULL, 
+             'leve'), 
+            (NULL, 
+             'moderada'), 
+            (NULL, 
+             'severa'), 
+            (NULL, 
+             'grave'), 
+            (NULL, 
+             'muy grave'), 
+            (NULL, 
+             'crítica'), 
+            (NULL, 
+             'fatal'), 
+            (NULL, 
+             'mortal'), 
+            (NULL, 
+             'desconocida')
