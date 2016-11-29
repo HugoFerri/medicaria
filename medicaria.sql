0bd00b344d3ed3bf2faaae3f5d88d6057c21819f
a003153722p <a003153722p@s09-pc03>
2016-11-29 16:11:43 +0100

    Creada tabla medico

--- medicaria.sql
+++ medicaria.sql
@@ -3,4 +3,78 @@
 -- ---------------------------------
 -- Sistema de información Sanitario
 -- ---------------------------------
+-- phpMyAdmin SQL Dump
+-- version 4.6.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Tiempo de generación: 29-11-2016 a las 07:07:56
+-- Versión del servidor: 5.6.32
+-- Versión de PHP: 5.6.25
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `medicaria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `medico`
+--
+
+CREATE TABLE `medico` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `primer_apellido` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `segundo_apellido` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `id_servicio` int(11) NOT NULL,
+  `id_especialidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `medico`
+--
+
+INSERT INTO `medico` (`id`, `nombre`, `primer_apellido`, `segundo_apellido`, `id_servicio`, `id_especialidad`) VALUES
+(1, 'Adrian', 'Rodriguez', 'Yanez', 1, 1),
+(2, 'Francisco', 'Rodriguez', 'Garcia', 2, 5),
+(3, 'Fernando', 'Gonzalez', 'Castro', 3, 7),
+(4, 'Antonio', 'Ruiz', 'Hernandez', 4, 4),
+(5, 'Miguel', 'Sanchez', 'Iglesias', 7, 8),
+(6, 'Rafael', 'Batistuta', 'Jimenez', 6, 2),
+(7, 'Jose', 'Perez', 'Gomez', 2, 2),
+(8, 'Alvaro', 'Medina', 'Rodriguez', 6, 6),
+(9, 'Juana', 'Jimenez', 'Rodriguez', 4, 4),
+(10, 'Cristina', 'Fernandez', 'Hernandez', 5, 5);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `medico`
+--
+ALTER TABLE `medico`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `medico`
+--
+ALTER TABLE `medico`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
